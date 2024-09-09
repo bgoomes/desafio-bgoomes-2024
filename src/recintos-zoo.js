965fdce447fdee6cd2389028a9e667a18100bebd
Bruno Gomes <obrunogomes@obrunogomes.local>
2024-09-09 11:01:40 -0300

    update constructor

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,11 +1,32 @@
-import { especies } from "./dados/animais.js";
-import { recintos } from "./dados/recintos.js";
+
 
 class RecintosZoo {
+    constructor(){
+        this.recintos=[
+            {numero:1,bioma:'savana',tamanhoTotal:10,animais:[{animal:'MACACO', quantidade:3}]},
+            {numero:2,bioma:'floresta', tamanhoTotal:5,animais:[]},
+            {numero:3,bioma:'savana e rio',tamanhoTotal:7, animais:[{animal:'GAZELA'}]},
+            {numero:4,bioma:' rio', tamanhoTotal:8, animais:[ ]},
+            {numero: 5 , bioma:'savana', tamanhoTotal:9, animais:[{animal:'LEAO'}]}
+
+
+        ];
+
+        this.animais={
+            LEAO:{tamanho:3,bioma:['savana'], carnivoro:true},
+            LEOPARDO:{tamanho:2,bioma:['savana'], carnivoro:true},
+            MACACO:{tamanho:1,bioma:['savana', 'floresta'], carnivoro:false},
+            GAZELA:{tamanho:2,bioma:['savana' ], carnivoro:false},
+            HIPOPOTAMO:{tamanho:4,bioma:['savana', 'rio'], carnivoro:false},
+            CROCODILO: { tamanho: 3, bioma: ['rio'], carnivoro: true }
+        }
+    }
 
     analisaRecintos(animal, quantidade) {
         
     }
+
+    
 
 }
 
